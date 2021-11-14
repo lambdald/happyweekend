b153774e4e4fa61b6fa939a35143db37c999d3db
lambdald <lambdald@163.com>
2021-11-14 17:13:03 +0800

    gcy 666

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,13 @@
 '''
-Author: lidong57
+Author: lambdald
 Date: 2021-11-14 17:00:18
-LastEditors: lidong57
+LastEditors: lambdald
 Description: file content
 '''
 
 def main():
     print("Hello World!")
+
+if __name__ == "__main__":
+    print("gcy 666")
+    main()
